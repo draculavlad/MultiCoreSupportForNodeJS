e8b364652122d6ff49b5b963a96470cb77bf6e19
draculavlad <450416583@qq.com>
2015-11-17 11:24:52 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-
+var express = require('express');
+var port = 80;
+var app = express();
+app.use(express.bodyParser());
+app.get('/', function(req, res){	
+ res.status(200);
+ res.end("hello world\n");
+});
+app.listen(port,function(){
+	console.log('Listening on port ' + port);
+});
